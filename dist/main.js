8fe31b2c812388d2ab3f8f08dcb87ae715152b0e
leochen9986 <92980109+leochen9986@users.noreply.github.com>
2024-10-20 23:06:43 +0800

    main

--- dist/main.js
+++ dist/main.js
@@ -30,7 +30,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, options);
     swagger_1.SwaggerModule.setup('api', app, document);
-    await app.listen(process.env.PORT || 3000);
+    await app.listen(process.env.PORT || 3000, '0.0.0.0');
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
